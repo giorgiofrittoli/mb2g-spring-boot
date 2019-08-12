19d967d0aa895f51f5ef5a54b62b0903be83fd84
frigir <giorgio.frittoli@gmail.com>
2019-08-12 15:04:01 +0200

    init repo

---
+++ src/test/java/it/frigir/mb2gspringboot/Mb2gSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package it.frigir.mb2gspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mb2gSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
